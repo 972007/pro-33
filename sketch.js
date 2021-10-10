935484f17afb060d30b597fa63518a2231412925
972007 <88716461+972007@users.noreply.github.com>
2021-10-10 13:20:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+var background = "snow1.png";
+
+function setup() {
+  createCanvas(Windowwidth, Windowheight);
+   background
+}
+
+function draw() {
+  background(255,255,255);  
+ 
+  drawSprites();
+}
